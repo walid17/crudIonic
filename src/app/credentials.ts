ba6c2744151dce99c832bf41fc02a1545aeec414
walid17 <walidbenamor17@yahoo.fr>
2018-09-07 09:09:04 +0100

    update gitignore

--- src/app/credentials.ts
+++
@@ -1,8 +0,0 @@
-export var config = {
-    apiKey: "AIzaSyAVojMxtOsHfjK8Ao4JewVWIYSAQxHpBnE",
-    authDomain: "tuto-7cec2.firebaseapp.com",
-    databaseURL: "https://tuto-7cec2.firebaseio.com",
-    projectId: "tuto-7cec2",
-    storageBucket: "tuto-7cec2.appspot.com",
-    messagingSenderId: "751838631839"
-  };
