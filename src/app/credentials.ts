e7e8ba2dbbde90ce00b56b3f2bbfd2d4d29fccd2
walid17 <walidbenamor17@yahoo.fr>
2018-09-07 09:02:17 +0100

    first commit

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export var config = {
+    apiKey: "AIzaSyAVojMxtOsHfjK8Ao4JewVWIYSAQxHpBnE",
+    authDomain: "tuto-7cec2.firebaseapp.com",
+    databaseURL: "https://tuto-7cec2.firebaseio.com",
+    projectId: "tuto-7cec2",
+    storageBucket: "tuto-7cec2.appspot.com",
+    messagingSenderId: "751838631839"
+  };
